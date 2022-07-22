7ac78ab156cfb4ca0c7eecaac05fe1fd861c66fd
Dominic <dominicdeantonio@icloud.com>
2022-07-22 19:38:25 -0400

    Update cors.dart

--- lib/src/cors.dart
+++ lib/src/cors.dart
@@ -3,7 +3,10 @@ import 'package:cumulus/cumulus.dart';
 class Cors {
   static Route route = Route(
     path: 'any',
-    handler: (Context c) => c.response.headers['Access-Control-Allow-Origin'] = '*',
+    handler: (Context c) {
+      c.response.headers["Access-Control-Allow-Methods"] = "*";
+      c.response.headers['Access-Control-Allow-Origin'] = '*';
+    },
     method: HttpMethod.OPTIONS,
   );
 }
